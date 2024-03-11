a6e84b6569eb53a871223b0c2700f42268c9e958
MrRobot2002 <just.zubavicius@gmail.com>
2024-03-11 18:48:36 +0200

    work.

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -64,7 +64,7 @@ public class UserLoginTest {
 
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
